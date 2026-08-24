aca3982c28c6691155bdf98e32a6690e886b4e47
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-08-24 23:28:56 +0530

    added controllers for get all conversation , mark as read and delete massages

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -13,7 +13,7 @@ exports.sendMessage = async (req, res) => {
 
 
     // check if a conversation already exists between the participants
-    let conversation = await Conversation.findOne({ participants:participants });
+    let conversation = await Conversation.findOne({ participants: [senderId, receiverId] });
 
     if (!conversation) {
       // create a new conversation if it doesn't exist
@@ -49,9 +49,125 @@ exports.sendMessage = async (req, res) => {
       } else { return res.status(400).json({ error: 'Message content is required' });
     }
 
+    const message = new Message({
+      conversation:conversation._id,
+      sender:senderId,
+      receiver:receiverId,
+      content:content || null,
+      contentType:contentType,
+      imageOrVideoUrl:imageOrVideoUrl || null,
+      messageStatus:messageStatus || 'sent',
+    });
+    await message.save();
+
+    if(message?.content){
+      conversation.lastMessage = message?.id;
+    }
+    conversation.unreadCount += 1;
+    await conversation.save();
+
+    const populatedMessage = await Message.findById(message._id)
+      .populate('sender', 'username profilePicture')
+      .populate('receiver', 'username profilePicture');
+    res.status(201).json({ message: 'Message sent successfully', data: populatedMessage });
 
   } catch (error) {
     console.error('Error sending message:', error);
     res.status(500).json({ error: 'Failed to send message' });
+  }
+} 
+
+// get all conversations
+
+exports.getConversations = async (req, res) => {
+  const userId = req.user.userId;
+  try {
+    let conversations = await Conversation.find({
+       participants: userId
+    }).populate('participants', 'username profilePicture isOnline lastSeen')
+    .populate({
+      path: 'lastMessage',
+      populate: {
+        path: 'sender receiver',
+        select: 'username profilePicture'
+      }
+    })
+    .sort({ updatedAt: -1 });
+    return response.success(res, 200, 'Conversations fetched successfully', conversations);
+  } catch (error) {
+    console.error('Error fetching conversations:', error);
+    res.status(500).json({ error: 'Failed to fetch conversations' });
+  }
+}
+
+// get messages for a specific conversation
+
+exports.getMessages = async (req, res) => {
+  const { conversationId } = req.params;
+  const userId = req.user.userId;
+  try {
+    const conversation = await Conversation.findById(conversationId);
+    if (!conversation) {
+      return res.status(404).json({ error: 'Conversation not found' });
+    };
+    if (!conversation.participants.includes(userId)) {
+      return res.status(403).json({ error: 'You are not a participant in this conversation' });
+    }
+    const messages = await Message.find({ conversation: conversationId })
+      .populate('sender', 'username profilePicture')
+      .populate('receiver', 'username profilePicture')
+      .sort({ createdAt: 1 });
+
+      await Message.updateMany(
+        { conversation: conversationId, receiver: userId, messageStatus: 'sent' },
+        { $set: { messageStatus: 'read' } }
+      );
+
+      conversation.unreadCount = 0;
+      await conversation.save();
+    return response.success(res, 200, 'Messages fetched successfully', messages);
+
+  } catch (error) {
+    console.error('Error fetching messages:', error);
+    res.status(500).json({ error: 'Failed to fetch messages' });
+  }
+}
+
+exports.markAsRead = async (req, res) => {
+  const { messageId } = req.body;
+  const userId = req.user.userId;
+  try {
+    const message = await Message.findById(messageId);
+    if (!message) {
+      return res.status(404).json({ error: 'Message not found' });
+    }
+    if (message.receiver.toString() !== userId) {
+      return res.status(403).json({ error: 'You are not the receiver of this message' });
+    }
+    message.messageStatus = 'read';
+    await message.save();
+    return response.success(res, 200, 'Message marked as read successfully', message);
+  } catch (error) {
+    console.error('Error marking message as read:', error);
+    res.status(500).json({ error: 'Failed to mark message as read' });
+  }
+}
+
+exports.deleteMessage = async (req, res) => {
+  const { messageId } = req.params;
+  const userId = req.user.userId;
+  try {
+    const message = await Message.findById(messageId);
+    if (!message) {
+      return res.status(404).json({ error: 'Message not found' });
+    }
+    if (message.sender.toString() !== userId) {
+      return res.status(403).json({ error: 'You are not the sender of this message' });
+    }
+    await message.deleteOne();
+    return response.success(res, 200, 'Message deleted successfully', message);
+  } catch (error) {
+    console.error('Error deleting message:', error);
+    res.status(500).json({ error: 'Failed to delete message' });
   }
 }
